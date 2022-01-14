d97803a5d43e9b6b25905a4a4971d16e92d7984e
Mahesh Vangala <vangalamaheshh@gmail.com>
2022-01-14 16:25:33 -0500

    continue on input type mismatch

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -10,7 +10,13 @@ fn main() {
     println!("Enter a number: ");
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("Error!");
-    let guess: u32 = guess.trim().parse().expect("Please enter a number.");
+    let guess: u32 = match guess.trim().parse() {
+      Ok(num) => num,
+      Err(_) => {
+        println!("Entered value is not a number.");
+        continue;
+      },
+    };
     match guess.cmp(&secret) {
       Ordering::Less => println!("Too small."),
       Ordering::Greater => println!("Too big."),
