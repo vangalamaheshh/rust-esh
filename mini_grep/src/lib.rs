c2687f47161cfe7a4edd76cda7cc1378bd40460f
Mahesh Vangala <vangalamaheshh@gmail.com>
2022-01-21 10:37:36 -0500

    added tests

--- mini_grep/src/lib.rs
+++ mini_grep/src/lib.rs
@@ -22,3 +22,27 @@ pub fn run(config: Config) -> Result<(), Box<dyn error::Error>> {
     println!("{}", contents);
     Ok(())
 }
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut v = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            v.push(line);
+        }
+    }
+    v
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
