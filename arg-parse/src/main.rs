4fca53b5aace48e1d1dab5276769629aa778a8cf
Mahesh Vangala <vangalamaheshh@gmail.com>
2022-03-12 14:03:59 -0500

    argparse

--- arg-parse/src/main.rs
+++ arg-parse/src/main.rs
@@ -1,17 +1,27 @@
 extern crate argparse;
 use argparse::{ ArgumentParser, Store };
-use std::fs;
 
 #[derive(Debug)]
 struct Options {
     filename: String,
 }
 
+#[derive(Debug)]
+struct List(Vec<u8>);
+
+/// Reads file content into a giant String
 fn read_file_as_string(file: &str) -> Result<String, Box<dyn std::error::Error>> {
-    let data = fs::read_to_string(file)?;
+    let data = std::fs::read_to_string(file)?;
     Ok(data)
 }
 
+/// Reads file content into a vector of bytes
+fn read_file_as_list(file: &str) -> Result<List, Box<dyn std::error::Error>> {
+    let data = std::fs::read(file)?;
+    Ok(List(data))
+}
+
+/// Returns command line args
 fn parse_args() ->  Options {
     let mut filename = String::from("");
     {
@@ -26,8 +36,14 @@ fn parse_args() ->  Options {
 
 fn main() {
     let options: Options = parse_args();
+    eprintln!("Reading file content as String.");
     match read_file_as_string(&options.filename) {
         Ok(data) => println!("{}", data),
+        Err(e) => eprintln!("{}", e),
+    }
+    eprintln!("Reading file as Vector of bytes.");
+    match read_file_as_list(&options.filename) {
+        Ok(data) => println!("{:?}", data),
         Err(e) => eprintln!("{}", e),
     }
 }
